d82445d315dc7a99f03c49ba9bf585f72192da20
Joel Lord <joel.lord@hotmail.co.uk>
2015-03-05 10:45:48 +0000

    Initial setup and registers added

---
+++ Master System Tests/Z80Test.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Master_System.Hardware;
+
+namespace Master_System_Tests
+{
+	[TestClass]
+	public class Z80Test
+	{
+		private Z80 z80;
+
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			z80 = new Z80();
+
+			z80.A = 0x01;
+			z80.B = 0x02;
+			z80.C = 0x03;
+			z80.D = 0x04;
+			z80.E = 0x05;
+			z80.F = 0x06;
+			z80.H = 0x07;
+			z80.L = 0x08;
+		}
+
+		[TestMethod]
+		public void TestDualRegister()
+		{
+			var upper = (z80.DualRegister(z80.A, z80.F) & 0xFF00) >> 8;
+			var lower = (z80.DualRegister(z80.A, z80.F) & 0x00FF);
+
+			Assert.AreEqual(0x01, upper);
+			Assert.AreEqual(0x06, lower);
+		}
+
+		[TestMethod]
+		public void TestSetAF()
+		{
+			z80.SetAF(0x45F8);
+
+			Assert.AreEqual(0x45, z80.A);
+			Assert.AreEqual(0xF8, z80.F);
+		}
+
+		[TestMethod]
+		public void TestSetBC()
+		{
+			z80.SetBC(0x45F8);
+
+			Assert.AreEqual(0x45, z80.B);
+			Assert.AreEqual(0xF8, z80.C);
+		}
+
+		[TestMethod]
+		public void TestSetDE()
+		{
+			z80.SetDE(0x45F8);
+
+			Assert.AreEqual(0x45, z80.D);
+			Assert.AreEqual(0xF8, z80.E);
+		}
+
+		[TestMethod]
+		public void TestSetHL()
+		{
+			z80.SetHL(0x45F8);
+
+			Assert.AreEqual(0x45, z80.H);
+			Assert.AreEqual(0xF8, z80.L);
+		}
+
+		[TestMethod]
+		public void TestSetAF1()
+		{
+			z80.SetAF1(0x45F8);
+
+			Assert.AreEqual(0x45, z80.A1);
+			Assert.AreEqual(0xF8, z80.F1);
+		}
+
+		[TestMethod]
+		public void TestSetBC1()
+		{
+			z80.SetBC1(0x45F8);
+
+			Assert.AreEqual(0x45, z80.B1);
+			Assert.AreEqual(0xF8, z80.C1);
+		}
+
+		[TestMethod]
+		public void TestSetDE1()
+		{
+			z80.SetDE1(0x45F8);
+
+			Assert.AreEqual(0x45, z80.D1);
+			Assert.AreEqual(0xF8, z80.E1);
+		}
+
+		[TestMethod]
+		public void TestSetHL1()
+		{
+			z80.SetHL1(0x45F8);
+
+			Assert.AreEqual(0x45, z80.H1);
+			Assert.AreEqual(0xF8, z80.L1);
+		}
+	}
+}
---
+++ Master System/Hardware/Z80.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_System.Hardware
+{
+	public class Z80
+	{
+		ushort opcode;
+
+		ushort PC;
+
+		ushort SP;
+
+		ushort IX, IY;
+
+		public byte A, B, C, D, E, F, H, L;
+		public byte A1, B1, C1, D1, E1, F1, H1, L1;
+
+		byte I;
+
+		byte R;
+
+		#region RegisterGetSet
+		
+		public ushort DualRegister(byte upperRegister, byte lowerRegister)
+		{
+			return (ushort)((upperRegister << 8) | lowerRegister);
+		}
+
+		public void SetAF(ushort nnnn)
+		{
+			A = (byte)((nnnn & 0xFF00) >> 8);
+			F = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetBC(ushort nnnn)
+		{
+			B = (byte)((nnnn & 0xFF00) >> 8);
+			C = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetDE(ushort nnnn)
+		{
+			D = (byte)((nnnn & 0xFF00) >> 8);
+			E = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetHL(ushort nnnn)
+		{
+			H = (byte)((nnnn & 0xFF00) >> 8);
+			L = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetAF1(ushort nnnn)
+		{
+			A1 = (byte)((nnnn & 0xFF00) >> 8);
+			F1 = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetBC1(ushort nnnn)
+		{
+			B1 = (byte)((nnnn & 0xFF00) >> 8);
+			C1 = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetDE1(ushort nnnn)
+		{
+			D1 = (byte)((nnnn & 0xFF00) >> 8);
+			E1 = (byte)(nnnn & 0x00FF);
+		}
+
+		public void SetHL1(ushort nnnn)
+		{
+			H1 = (byte)((nnnn & 0xFF00) >> 8);
+			L1 = (byte)(nnnn & 0x00FF);
+		}
+
+		#endregion
+	}
+}
